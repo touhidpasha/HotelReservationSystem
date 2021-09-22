524bca17ac07531ccd6d1f8bbcc4f15c22275d02
touhidpasha <touhidpasha552@gmail.com>
2021-09-22 08:21:59 +0530

    hotelreservation system project created

---
+++ src/test/java/myPackage/HotelTestCase.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HotelTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
